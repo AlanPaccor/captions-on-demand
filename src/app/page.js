761caaa0d2cbb69122f6df4372ba39dfe2e07a5f
Alan Paccor <alan@Alans-Air.lan1>
2023-11-06 14:08:30 -0500

    done with landing page and pricing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
+import DemoSection from "./components/DemoSection";
+import PageHeaders from "./components/PageHeaders";
+import UploadIcon from "./components/uploadIcon";
+
 export default function Home() {
   return (
-    <div>test</div>
+    <>
+      <PageHeaders h1Text={'Add Captions on Demand to your videos'}
+      h2Text={'Just upload your video and we will do the rest'} />
+      <div className="text-center">
+        <button className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50">
+          <UploadIcon/>
+          <span>Choose file</span>
+          </button>
+      </div>
+      <DemoSection/>
+    </>
   )
 }
